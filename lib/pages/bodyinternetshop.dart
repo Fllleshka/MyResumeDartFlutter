336e799b047695d12a7175eb3675eb91a159584a
Fleshka <fleisner.omsk@gmail.com>
2021-11-22 00:49:03 +0600

    Add bags cards on screen. Insert dates from file.

--- lib/pages/bodyinternetshop.dart
+++ lib/pages/bodyinternetshop.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:myresume/models/product.dart';
 import 'package:myresume/pages/constants.dart';
+import 'package:myresume/pages/categoriesinternetshop.dart';
+import 'package:myresume/pages/itemcardinternetshop.dart';
 
 class BodyIntShop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
           padding: EdgeInsets.symmetric(horizontal: kDefaultPaddin / 4),
@@ -16,40 +20,24 @@ class BodyIntShop extends StatelessWidget {
           ),
         ),
         Categories(),
+        Expanded(
+            child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+                child: GridView.builder(
+                  itemCount: products.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: kDefaultPaddin,
+                    crossAxisSpacing: kDefaultPaddin,
+                    childAspectRatio: 0.75
+                  ),
+                  itemBuilder: (context, index) => ItemCard(
+                    product: products[index],
+                  ),
+                ),
+            ),
+        ),
       ],
     );
   }
 }
-
-class Categories extends StatefulWidget {
-  @override
-  _CategoriesState createState() => _CategoriesState();
-}
-class _CategoriesState extends State<Categories>{
-  List<String> categories = ["Офисные сумки","Поясные сумки","Тоут сумки","Хит 2000"];
-  int selectedIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 25,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: categories.length,
-        itemBuilder: (context, index) => buildCategoty(index),
-      ),
-    );
-  }
-  Widget buildCategoty (int index) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-      child: Text(
-        categories[index],
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: kTextColor,
-        ),
-      ),
-    );
-  }
-}
-
