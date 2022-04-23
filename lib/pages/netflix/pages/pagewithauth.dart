43c26251ca28f8e3ba52ebb250b806b6cbfed0e2
Fllleshka <fleisner.omsk@gmail.com>
2022-04-24 00:18:29 +0600

    Add change poster. Need realise swiper in posters or no)

--- lib/pages/netflix/pages/pagewithauth.dart
+++ lib/pages/netflix/pages/pagewithauth.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import 'package:myresume/pages/netflix/pages/widgets/castomappbar.dart';
 
 class PageWithAuth extends StatelessWidget {
@@ -11,7 +12,60 @@ class PageWithAuth extends StatelessWidget {
       color: Colors.white,
       fontWeight: FontWeight.bold,
     );
+    var imagesForDecoration = [
+      'assets/images/netflix/spiderman/1.jpg',
+      'assets/images/netflix/spiderman/2.jpg',
+      'assets/images/netflix/spiderman/3.jpg',
+      'assets/images/netflix/spiderman/4.jpg',
+    ];
 
+    Container _poster (String asset){
+      return Container(
+        height: 500,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image:  DecorationImage(
+            image: AssetImage(asset),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black, Colors.transparent],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+        ),
+      );
+    }
+    GestureDetector _ontapposter (String asset){
+      return GestureDetector(
+        onTap: (){
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                      contentPadding: EdgeInsets.all(0),
+                      content:
+                      Container(
+                        height: 500,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          image:  DecorationImage(
+                            image: AssetImage(asset),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                  );
+            },
+          );
+        },
+        child: _poster(asset),
+      );
+    }
 
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -35,27 +89,16 @@ class PageWithAuth extends StatelessWidget {
                 width: MediaQuery.of(context).size.width,
                 color: Colors.orange
             ),
+            // Постер фильма
             Positioned(
-                top: 0,
-                child:  Container(
-                  height: 500,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    image:  DecorationImage(
-                      image: AssetImage('assets/images/netflix/spiderman/1.jpg'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [Colors.black, Colors.transparent],
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                      ),
-                    ),
-                  ),
-                ),
+              top: 0,
+              child:  _ontapposter(imagesForDecoration[3]),
+              /*Swiper (
+                itemCount: imagesForDecoration.length,
+                itemBuilder: (BuildContext context, int index){
+                  return _ontapposter(imagesForDecoration[index]);
+                },
+              ),*/
             ),
             // Название фильма 1
             Positioned(
@@ -169,153 +212,6 @@ class PageWithAuth extends StatelessWidget {
           ]
         ),
       ),
-
-      /*SingleChildScrollView(
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            // Картинка
-            Positioned(
-              top: 0,
-              child: Container(
-                height: 500,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  image:  DecorationImage(
-                    image: AssetImage('assets/images/netflix/spiderman/1.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.black, Colors.transparent],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            // Название фильма 1
-            Positioned(
-              top: 350,
-              child: Container(
-                child: Text(
-                  'Spider-Man',
-                  style: TextStyle (
-                      fontSize: 30,
-                      fontFamily: 'FarFromHome',
-                      letterSpacing: 2,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue
-                  ),
-                ),
-              ),
-            ),
-            // Название фильма 2
-            Positioned(
-              top: 385,
-              child: Container(
-                child: Text(
-                  'from away to home',
-                  style: TextStyle (
-                      fontSize: 20,
-                      fontFamily: 'FarFromHome',
-                      letterSpacing: 2,
-                      color: Colors.white
-                  ),
-                ),
-              ),
-            ),
-            // Жанры фильма
-            /*Container(
-            margin: EdgeInsets.only(top: 410),
-            //decoration: BoxDecoration(color: Colors.orange),
-            child: Row (
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('фантастика', style: _textstyleforgenre),
-                Text('боевик', style: _textstyleforgenre),
-                Text('приключения', style: _textstyleforgenre),
-              ],
-            ),
-          ),*/
-            // Кнопки
-            Positioned(
-              top: 420,
-              left: 0,
-              right: 0,
-              child:
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // Левая кнопка List
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.add,
-                        color: Colors.white,
-                      ),
-                      SizedBox(height: 2.0),
-                      Text(
-                        'List',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
-                    ],
-                  ),
-                  // Кнопка
-                  TextButton.icon(
-                    onPressed: (){},
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      padding: EdgeInsets.only(left: 10, right: 15),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)
-                      ),
-                    ),
-                    icon: Icon(Icons.play_arrow, size: 30),
-                    label: Text(
-                      'Play',
-                      style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w600
-                      ),
-                    ),
-                  ),
-                  // Правая кнопка Info
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.info,
-                        color: Colors.white,
-                      ),
-                      SizedBox(height: 2.0),
-                      Text(
-                        'Info',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            // Популрные фильмы
-            Container(
-                margin: EdgeInsets.only(top: 600),
-                height: 500,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.orange
-            ),
-          ],
-        ),
-      )*/
     );
   }
 }
